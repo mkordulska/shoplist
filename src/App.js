53e5c61b08f704649965e901061dec6e1db4e3f9
unknown <m.kordulska@gmail.com>
2018-01-21 15:23:50 +0100

    react table added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactTable from 'react-table';
+import "react-table/react-table.css";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      lists: [{
+        name:'New',
+        data: [{
+            list: 'Grosery'
+          },{
+            list: 'Clothing'
+        }],
+        columns: [{
+          Header: 'List',
+          headerClassName: 'Table-header',
+          accessor: 'list'
+        },{
+          Header: 'Delete',
+          headerClassName: 'Table-header',
+          accessor: 'delete',
+          maxWidth: 100
+        },{
+          Header: 'Archive',
+          headerClassName: 'Table-header',
+          accessor: 'archive',
+          maxWidth: 100
+        }]
+      },{
+        name: 'Archived',
+        data: [{
+            list: 'Pharmacy'
+          }],
+        columns: [{
+          Header: 'List',
+          headerClassName: 'Table-header',
+          accessor: 'list'
+        },{
+          Header: 'Delete',
+          headerClassName: 'Table-header',
+          accessor: 'delete',
+          maxWidth: 100
+        }]
+      }]
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Shoplists</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.lists.map(function(list){
+          return (
+            <div>
+              <h1>{list.name}</h1>
+              <ReactTable noDataText="No entries!" pageSize="5" data={list.data} columns={list.columns}/>
+            </div>
+          );
+        })}
       </div>
     );
   }
